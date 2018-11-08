3459221a91ba198715a2e5dc7ceb2be7a861987a
Chris Laupama <chrislaupama@live.com>
2018-11-09 12:48:42 +1300

    Added routes for dynamic links and linked to components

--- client/components/App.jsx
+++ client/components/App.jsx
@@ -2,6 +2,8 @@ import React from 'react'
 import { HashRouter as Router, Route } from 'react-router-dom'
 import Home from './Home'
 import Nav from './Nav'
+import List from './List'
+import Item from './Item'
 
 const App = () => {
   return (
@@ -9,6 +11,8 @@ const App = () => {
       <div>
         <Route exact path='/' component={Home} />
         <Route path='/' component={Nav} />
+        <Route path='/:list' component={List} />
+        <Route path='/:list/:item' component={Item} />
       </div>
     </Router>
   )
