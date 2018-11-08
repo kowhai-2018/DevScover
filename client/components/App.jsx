7679ac4648566509bdc102a804b655108fa4cc44
Alina Kireeva <alina.kireeva88@gmail.com>
2018-11-09 12:10:17 +1300

    added index.html

--- client/components/App.jsx
+++ client/components/App.jsx
@@ -1,18 +1,8 @@
 import React from 'react'
-import { HashRouter as Router, Route } from 'react-router-dom'
-import Home from './Home'
-// import Nav from './Nav'
-// import List from './List'
-// import Item from './Item'
 
 const App = () => {
   return (
-    <Router>
-      <div>
-        <Route exact path='/' component={Home} />
-        {/* <Router path='/list' component={List} /> */}
-      </div>
-    </Router>
+    <h1>DevScover</h1>
   )
 }
 
