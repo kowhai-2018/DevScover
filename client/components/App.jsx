2844c5287da7bf21edeb7f3d2b4ddc5d13784922
Alina Kireeva <alina.kireeva88@gmail.com>
2018-11-09 11:47:22 +1300

    data imported into data file

--- client/components/App.jsx
+++ client/components/App.jsx
@@ -1,9 +1,18 @@
-
 import React from 'react'
+import { HashRouter as Router, Route } from 'react-router-dom'
+import Home from './Home'
+// import Nav from './Nav'
+// import List from './List'
+// import Item from './Item'
 
 const App = () => {
   return (
-    <h1>React development has begun!</h1>
+    <Router>
+      <div>
+        <Route exact path='/' component={Home} />
+        {/* <Router path='/list' component={List} /> */}
+      </div>
+    </Router>
   )
 }
 
---
+++ data/data.js
@@ -0,0 +1,68 @@
+const data = {
+  FrontendLibraries: [
+    {
+      id: 1,
+      name: 'Skeleton',
+      description: 'Skeleton is a simple, responsive boilerplate to kickstart any responsive project.',
+      link: 'http://getskeleton.com/'
+    },
+    {
+      id: 2,
+      name: 'Bootstrap',
+      description: 'Bootstrap is an open source toolkit for developing with HTML, CSS, and JS. Quickly prototype your ideas or build your entire app with our Sass variables and mixins, responsive grid system, extensive prebuilt components, and powerful plugins built on jQuery.',
+      link: 'https://getbootstrap.com/'
+    }
+  ],
+  BackEndLibraries: [
+    {
+      id: 1,
+      name: 'Express',
+      description: 'Express is a minimal and flexible Node.js web application framework that provides a robust set of features for web and mobile applications.',
+      link: 'https://expressjs.com/'
+    },
+    {
+      id: 2,
+      name: 'Bootstrap',
+      description: 'Bootstrap is an open source toolkit for developing with HTML, CSS, and JS. Quickly prototype your ideas or build your entire app with our Sass variables and mixins, responsive grid system, extensive prebuilt components, and powerful plugins built on jQuery.',
+      link: 'https://getbootstrap.com/'
+    }
+  ],
+  DatabaseLibraries: [
+    {
+      id: 1,
+      name: 'Knex.js',
+      description: 'Knex.js is a "batteries included" SQL query builder for Postgres, MSSQL, MySQL, MariaDB, SQLite3, Oracle, and Amazon Redshift designed to be flexible, portable, and fun to use.',
+      link: 'https://knexjs.org/'
+    }
+  ],
+  TestingLibraries: [
+    {
+      id: 1,
+      name: 'Jest',
+      description: 'Jest is a library for testing JavaScript code. It\'s an open source project maintained by Facebook, and it\'s especially well suited for React code testing, although not limited to that: it can test any JavaScript code.',
+      link: 'https://jestjs.io/'
+    }
+  ],
+  Languages: [
+    {
+      id: 1,
+      name: 'HTML',
+      description: 'Hypertext Markup Language (HTML) is the standard markup language for creating web pages and web applications. ',
+      link: 'https://www.w3.org/html/'
+    },
+    {
+      id: 2,
+      name: 'CSS',
+      description: 'Cascading Style Sheets (CSS) is a style sheet language used for describing the presentation of a document written in a markup language like HTML.',
+      link: 'https://www.w3.org/Style/CSS/'
+    },
+    {
+      id: 3,
+      name: 'JavaScript',
+      description: ' JavaScript enables interactive web pages and thus is an essential part of web applications. The vast majority of websites use it, and all major web browsers have a dedicated JavaScript engine to execute it.',
+      link: 'https://developer.mozilla.org/en-US/docs/Web/JavaScript'
+    }
+  ]
+}
+
+module.exports = data
