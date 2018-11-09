a5ef198ec1150969632f221a2d5c9e9ab66530c9
Chris Laupama <chrislaupama@live.com>
2018-11-09 13:57:44 +1300

    Got line 9 working, now to get it to display the array

--- client/components/List.jsx
+++ client/components/List.jsx
@@ -5,19 +5,19 @@ import data from '../../data/data'
 export default class LibraryList extends React.Component {
     render () {
         const { match } = this.props
-        const { category } = match.params
-        const libs = data[category]
+        const { list } = match.params
+        const libs = data[list]
       
-        console.log(libs)
+        console.log(libs[0])
       
         return (
           <div>
-              <h1>{library}</h1>
+              <h1>{list}</h1>
             <nav>
               {libs.map(lib => {
                 return (
                   <li>
-                    <Link to={`/${library}/${lib.name}`}> {lib.name} </Link>
+                    <Link to={`/${list}/${lib.name}`}> {lib.name} </Link>
                   </li>
                 )
               })}
